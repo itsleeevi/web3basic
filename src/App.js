dbd39c6c32811e8fae6331c4ecbf7cf125a677ae
Levente Kecskemeti <levi@DESKTOP-JL8CFLU.home>
2022-01-08 23:01:20 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import { Grommet, Button } from "grommet";
+import { grommet } from "grommet/themes";
+import Web3 from "web3";
 
 function App() {
+  const [connected, setConnected] = useState(false);
+  const [accounts, setAccounts] = useState([]);
+  const [web3, setWeb3] = useState(undefined);
+  const receiver = "0x62F46e2b272F0775C0872CF939800793FFe23654";
+
+  const connectMetaMask = async () => {
+    if (window.ethereum) {
+      const accs = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccounts(accs);
+      if (accs.length > 0) setConnected(true);
+      else setConnected(false);
+    } else {
+      alert(
+        "MetaMask is not installed. Please consider installing it: https://metamask.io/download.html"
+      );
+    }
+  };
+
+  const transfer = async () => {
+    if (web3)
+      await web3.eth.sendTransaction({
+        to: receiver,
+        from: accounts[0],
+        value: web3.utils.toWei("0.5", "ether"),
+      });
+  };
+
+  useEffect(() => {
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+      setWeb3(web3);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet style={{ backgroundColor: "#000" }} full theme={grommet}>
+      {!connected ? (
+        <Button
+          primary
+          label="Connect Wallet"
+          onClick={() => connectMetaMask()}
+        />
+      ) : (
+        <Button primary label="Transfer" onClick={() => transfer()} />
+      )}
+    </Grommet>
   );
 }
 
