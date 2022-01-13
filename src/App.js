903616dbc717ae7d99fea435e717ee17c8357b23
Levente Kecskemeti <levi@DESKTOP-JL8CFLU.home>
2022-01-13 19:50:25 +0100

    number input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
 
-import { Grommet, Button } from "grommet";
+import { Grommet, Button, Box, Grid, Heading } from "grommet";
 import { grommet } from "grommet/themes";
 import Web3 from "web3";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from "@chakra-ui/react";
 
 function App() {
   const [connected, setConnected] = useState(false);
   const [accounts, setAccounts] = useState([]);
   const [web3, setWeb3] = useState(undefined);
   const receiver = "0x62F46e2b272F0775C0872CF939800793FFe23654";
+  const [amount, setAmount] = useState("1");
+  const parse = (val) => val.replace(/^\$/, "");
+  const [value, setValue] = useState(1);
 
   const connectMetaMask = async () => {
     if (window.ethereum) {
@@ -30,7 +41,7 @@ function App() {
       await web3.eth.sendTransaction({
         to: receiver,
         from: accounts[0],
-        value: web3.utils.toWei("0.5", "ether"),
+        value: web3.utils.toWei(value, "ether"),
       });
   };
 
@@ -41,17 +52,73 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    setValue(Number(amount) * 10);
+  }, [amount]);
+
   return (
-    <Grommet style={{ backgroundColor: "#000" }} full theme={grommet}>
-      {!connected ? (
-        <Button
-          primary
-          label="Connect Wallet"
-          onClick={() => connectMetaMask()}
-        />
-      ) : (
-        <Button primary label="Transfer" onClick={() => transfer()} />
-      )}
+    <Grommet full theme={grommet}>
+      <ChakraProvider>
+        <Grid
+          fill={true}
+          pad="medium"
+          rows={["full"]}
+          columns={["auto", "flex", "auto"]}
+          gap="small"
+          areas={[
+            { name: "left", start: [0, 0], end: [0, 0] },
+            { name: "center", start: [1, 0], end: [1, 0] },
+            { name: "right", start: [2, 0], end: [2, 0] },
+          ]}
+        >
+          <Box gridArea="left" />
+          <Box
+            direction="column"
+            align="center"
+            justify="center"
+            alignSelf="center"
+            gridArea="center"
+            gap="medium"
+          >
+            {!connected ? (
+              <Button
+                primary
+                label="Connect Wallet"
+                onClick={() => connectMetaMask()}
+              />
+            ) : (
+              <Box
+                direction="column"
+                width="medium"
+                justify="center"
+                gap="small"
+              >
+                <NumberInput
+                  defaultValue={1}
+                  min={1}
+                  onChange={(valueString) => setAmount(parse(valueString))}
+                  value={amount}
+                >
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+                <Box
+                  border={{ color: "black", size: "large" }}
+                  pad="medium"
+                  align="center"
+                >
+                  <Heading>{value}</Heading>
+                </Box>
+                <Button primary label="Transfer" onClick={() => transfer()} />
+              </Box>
+            )}
+          </Box>
+          <Box gridArea="right" />
+        </Grid>
+      </ChakraProvider>
     </Grommet>
   );
 }
